251717b047ad7aa1ebbb067bcbf27e4d5467f072
Shayan-Chashm-Jahan <shayan.chashmjahan@gmail.com>
2024-06-18 15:28:55 -0400

    Updating the UI

--- app.py
+++ app.py
@@ -25,22 +25,25 @@ if __name__ == "__main__":
     #Streamlit UI
 
     st.title("Art Assistant")
-    st.write("Type 'exit' to end the conversation")
+    st.write("Welcome to the Movies&Songs AI Assistant.")
 
     if 'conversation_history' not in st.session_state:
         st.session_state.conversation_history = [
              {"role": "system", "content": "You are an assistant specializing in movies and songs. Only respond to queries related to movies and songs. Avoid any other topics."}
         ]
 
-    print("Welcome to the OpenAI Assistant. Type 'exit' to end the conversation")
+    user_input = st.text_input("You: ")
 
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() == 'exit':
-            break
-
+    if user_input:
         st.session_state.conversation_history.append({"role": "user", "content": user_input})
         response = generate_response(st.session_state.conversation_history)
 
         st.session_state.conversation_history.append({"role": "assistant", "content": response})
-        print(f"Assistant: {response}")
+        
+        st.write(f"Assistant: {response}")
+
+        for message in st.session_state.conversation_history:
+            if message['role'] == 'assistant':
+                st.write(f"Assistant: {message['content']}")
+            elif message['role'] == 'user':
+                st.write(f"You: {message['content']}")
